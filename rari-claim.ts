cf9a1cde1c9bbc22ed31cf362a88a598bdc69d5a
Eiman <eabdelmoneim@gmail.com>
2024-02-05 11:42:10 -0600

    added rari-claim

---
+++ rari-claim.ts
@@ -0,0 +1,25 @@
+import { ThirdwebSDK, getDefaultTrustedForwarders, predictThirdwebContractAddress } from "@thirdweb-dev/sdk";
+import { Rari} from "@thirdweb-dev/chains";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+
+const run = async () => {
+
+// connect to the SDK with the wallet using the relayer URL for the Mumbai testnet
+const sdk = ThirdwebSDK.fromPrivateKey(process.env.THIRDWEB_ADMIN_PRIVATE_KEY as string, Rari,{secretKey: process.env.THIRDWEB_SECRET_KEY as string});
+
+const contract = await sdk.getContract("0xf82427790E0c4E0C9a701655b53C8708d5FeFb19");
+
+
+const tx = await contract.erc721.claimTo("0x749CaA9A7bbF7D5aEb8Ea6E92335AFa2f74dE4EE", 1);
+console.log(tx);
+
+};
+run()
+  .then(() => process.exit(0))
+  .catch((err) => {
+    console.error(err);
+    process.exit(1);
+  });
--- simple-reads.ts
+++ simple-reads.ts
@@ -11,7 +11,7 @@ const run = async () => {
 // connect to the SDK with the wallet using the relayer URL for the Mumbai testnet
 const sdk = ThirdwebSDK.fromPrivateKey(process.env.THIRDWEB_ADMIN_PRIVATE_KEY as string, BaseSepoliaTestnet,{secretKey: process.env.THIRDWEB_SECRET_KEY as string});
 
-const contract = await sdk.getContract("0xC5e6BbEc31F690BCc4E95E36f3f2Cf7f2FdEDF1c");
+const contract = await sdk.getContract("0xC5e6BbEc31F690BCc4E95E36f3f2Cf7f2FdEDF1c", );
 
 const balance = await contract.erc721.balanceOf("0x749CaA9A7bbF7D5aEb8Ea6E92335AFa2f74dE4EE");
 console.log(balance.toNumber());
