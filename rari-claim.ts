e9b2f4093285e59e82cca1ea8cc89fb61d55c1d8
Eiman <eabdelmoneim@gmail.com>
2024-02-05 11:43:03 -0600

    removed comment

--- rari-claim.ts
+++ rari-claim.ts
@@ -7,11 +7,9 @@ dotenv.config();
 
 const run = async () => {
 
-// connect to the SDK with the wallet using the relayer URL for the Mumbai testnet
 const sdk = ThirdwebSDK.fromPrivateKey(process.env.THIRDWEB_ADMIN_PRIVATE_KEY as string, Rari,{secretKey: process.env.THIRDWEB_SECRET_KEY as string});
 
 const contract = await sdk.getContract("0xf82427790E0c4E0C9a701655b53C8708d5FeFb19");
-
 
 const tx = await contract.erc721.claimTo("0x749CaA9A7bbF7D5aEb8Ea6E92335AFa2f74dE4EE", 1);
 console.log(tx);
